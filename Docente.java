a9ae5234363c43a55d5b79de9468bc6a66e97664
Camilo N NUNCIRA <90908546+CamiloENunciraN@users.noreply.github.com>
2022-09-03 02:00:18 +0000

    Initial commit

---
+++ Docente.java
@@ -0,0 +1,19 @@
+class Docente {
+private string codigo;
+private string nombre;
+private string apellido;
+private string profesion;
+private float salario;
+private float estatura;
+private float peso;
+
+
+Docente(){
+  
+}
+
+
+
+
+  
+}
